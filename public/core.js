e4d60a02316978505308891f36f9f332ebed7ff0
Kodah <tom_1440@hotmail.co.uk>
2016-05-24 09:11:46 +0100

    Add loading bar

--- public/core.js
+++ public/core.js
@@ -2,9 +2,10 @@ angular.module('app', []).controller('MainController', ['$scope', '$http',
     function($scope, $http) {
 
         $scope.successfulInfo = false;
-
+        $scope.isProcessing = false;
         $scope.update = function(youtubeURL) {
 
+            $scope.isProcessing = true;
             $http({
                 method: 'POST',
                 url: '/api/mp3',
@@ -13,9 +14,11 @@ angular.module('app', []).controller('MainController', ['$scope', '$http',
                 $scope.successfulInfo = true;
                 $scope.videoInfo = response.data[0];
                 console.log($scope.videoInfo);
+                $scope.isProcessing = false;
             }, function errorCallback(response) {
                 $scope.successfulInfo = false;
                 console.log(response);
+                $scope.isProcessing = false;
             });
 
         };
