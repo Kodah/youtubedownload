0bc297066d53d5ee9043780cd5a9d3784b8ef5f2
Tom Sugarev <Kodah@users.noreply.github.com>
2016-09-21 16:50:35 +0100

    profanity removal

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -2,6 +2,6 @@ angular.module('controllers/mainController', [])
 
 	// inject the Todo service factory into our controller
 	.controller('mainController', ['$scope', function($scope) {
-		$scope.fanny = 'fanny'
+		$scope.bacon = 'bacon'
 		
 	}]);
